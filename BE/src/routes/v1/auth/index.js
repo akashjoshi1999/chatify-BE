621dd4f0da2ddc5d180a5b020d3ae8e7f5ad0bab
akashjoshi1999 <akashjoshi61099@gmail.com>
2026-01-22 22:14:30 +0530

    fix: correct spelling of 'Success' in authentication routes and add message routes

--- BE/src/routes/v1/auth/index.js
+++ BE/src/routes/v1/auth/index.js
@@ -4,27 +4,27 @@ const router = express.Router();
 
 // /api/v1/auth/login POST Request
 router.post('/login', (req, res) => {
-    res.sendFile('Sucess');
+    res.sendFile('Success');
 });
 
 // /api/v1/auth/sign-up POST Request
 router.post('/sign-up', (req, res) => {
-    res.sendFile('Sucess');
+    res.sendFile('Success');
 });
 
 // /api/v1/auth/forgot-password POST Request
 router.post('/forgot-password', (req, res) => {
-    res.sendFile('Sucess');
+    res.sendFile('Success');
 });
 
 // /api/v1/auth/reset-password POST Request
 router.post('/reset-password', (req, res) => {
-    res.sendFile('Sucess');
+    res.sendFile('Success');
 });
 
 // /api/v1/auth/logout POST Request
 router.post('/logout', (req, res) => {
-    res.sendFile('Sucess');
+    res.sendFile('Success');
 });
 
 module.exports = router; 
--- BE/src/routes/v1/index.js
+++ BE/src/routes/v1/index.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 const authRoutes = require('./auth');
+const messageRoutes = require('./message');
 
+// to check the status of API
+router.get('/status', (req, res) => {
+    res.status(200).json({
+        message: 'OK'
+    })
+});
+
+// auth route : api/v1/auth/
 router.use('/auth', authRoutes);
+
+router.use('/message', messageRoutes);
 
 module.exports = router;
